40364b90039ce9eaa4bba482237f9a23e8f87868
Mr nova <mamali.azim@gmail.com>
2016-07-21 17:03:02 +0430

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -1,29 +1,24 @@
 do
 function run(msg)
 	send_document("chat#id"..msg.to.id,"./file/qr.webp", ok_cb, false)
-	return 'Umbrella Telegram Bot v2.0'.. [[ 
+	return 'blaster Telegram Bot v2.0'.. [[ 
 	
-	Website:
-	http://Umbrella.shayan-soft.IR
-	Antispam Bot : @UmbrellaTG
-	Channel : @UmbrellaTeam
-	Sudo : @shayansoft
+	Channel : @fucker_Team
+	Sudo : @nethall
 	
 	Powered by:
-	shayan soft Co. Group
-	Engineer Shayan Ahmadi
-	http://shayan-soft.IR
+        @nethall
 	
 	Special Thanks:
 	Uzzy
 	Yagop
 	Iman Daneshi
-	and more...]]
+	tg_break
 end
 
 return {
 	description = "Robot and Creator About", 
-	usagehtm = '<tr><td align="center">ver</td><td align="right">ارائه ی توضیحاتی در خصوص پروژه ی ربات آمبرلا و همچنین سازنده و توسئه دهنده ی آن</td></tr>',
+	usagehtm = '<tr><td align="center">ver</td><td align="right">ارائه ی توضیحاتی در خصوص پروژه ی رباتنواا و همچنین سازنده و توسئه دهنده ی آن</td></tr>',
 	usage = "ver : درباره ربات",
 	patterns = {
 		"^[Vv]er$"
